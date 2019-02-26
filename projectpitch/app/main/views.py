97330993051b796e8807cc18acf37cc9181de9ac
ingabire <ingabire@sylvie.gmail.com>
2019-02-26 18:40:58 +0200

    I added some file to  my application

--- projectpitch/app/main/views.py
+++ projectpitch/app/main/views.py
@@ -28,53 +28,6 @@ def index():
     #     return redirect(url_for('search',news_title=search_news))
     # else:
         return render_template('index.html', title = title,technology = technology_news,general = general_news, entertainment = entertainment_news )
-
-# @main.route('/news/<id>')
-# def newsd(id):
-
-#     '''
-#     View news page function that returns the news details page and its data
-#     '''
-#     technology_news = get_newsd(id)
-
-#     title = 'Home - Welcome to The Articles'
-
-#     search_news = request.args.get('news_query')
-
-#     if search_news:
-#         return redirect(url_for('search',news_title=search_news))
-#     else:
-#         return render_template('news.html', title = title,technology = technology_news)
-
-
-# @main.route('/search/<news_title>')
-# def search(news_title):
-#     '''
-#     View function to display the search results
-#     '''
-#     news_title_list = news_title.split(" ")
-#     news_title_format = "+".join(news_title_list)
-#     searched_news = search_news(news_title_format)
-#     title = f'search results for {news_title}'
-
-#     return render_template('search.html',news = searched_news)
-
-# @main.route('/news/review/new/<id>', methods = ['GET','POST'])
-# @login_required
-# def new_review(id):
-#     form = ReviewForm()
-#     news = get_newsd(id)
-
-#     if form.validate_on_submit():
-#         title = form.title.data
-#         review = form.review.data
-#         new_review = Review(news.id,title,news.poster,review)
-#         new_review.save_review()
-#         return redirect(url_for('news',id = news.id ))
-
-#     title = f'{news.title} review'
-#     return render_template('new_review.html',title = title, review_form=form, news=news)
-
 @main.route('/user/<uname>')
 def profile(uname):
     user = User.query.filter_by(username = uname).first()
