a9e9292e816ce829dcaf3fc402440aba83546cd7
ingabire <ingabire@sylvie.gmail.com>
2019-02-26 16:41:03 +0200

    Flask script allows us to create a Python shell inside our application

--- projectpitch/manage.py
+++ projectpitch/manage.py
@@ -20,8 +20,8 @@ def test():
 if __name__ == '__main__':
     manager.run()
 
-@manager.shell
-def make_shell_context():
-    return dict(app = app,db = db,User = User)
-if __name__ == '__main__':
-    manager.run()
+# @manager.shell
+# def make_shell_context():
+#     return dict(app = app,db = db,User = User)
+# if __name__ == '__main__':
+#     manager.run()
