4899674bd68e582c48efbe3d221060e7ccde0fe9
ingabire <ingabire@sylvie.gmail.com>
2019-02-27 12:18:15 +0200

    restart my project

--- projectpitch/start.sh
+++
@@ -1,7 +0,0 @@
-NEWS_API_KEY=ce629c831305410ca8237ce243de94a2
-SECRET_KEY=wtfformssecretkey
-
-export MAIL_USERNAME=<Your Email Address>
-export MAIL_PASSWORD=<Your Email Password>
-
-python3.6 manage.py server
