eb433421ca206c767efabfd50edf05a051e210c4
ingabire <ingabire@sylvie.gmail.com>
2019-02-27 20:03:42 +0200

    I create database named yvette

---
+++ projectpitch/app/__init__.py
@@ -0,0 +1,33 @@
+
+from flask_sqlalchemy import SQLAlchemy 
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+    db.init_app(app)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .request import configure_request
+    configure_request(app)
+    # Will add the views and forms
+   
+
+    return app
+
+
+    
